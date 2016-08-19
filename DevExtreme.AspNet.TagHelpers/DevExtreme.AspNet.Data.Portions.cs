565c942bba4bbfe687dc7e8c377bdd57bf558c8b
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2016-08-19 11:45:45 +0300

    revert revert unrelated changes

--- DevExtreme.AspNet.TagHelpers/DevExtreme.AspNet.Data.Portions.cs
+++ DevExtreme.AspNet.TagHelpers/DevExtreme.AspNet.Data.Portions.cs
@@ -1,37 +1,38 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using DevExtreme.AspNet.Data;
+﻿using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Data.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace DevExtreme.AspNet.TagHelpers
-{
+namespace DevExtreme.AspNet.TagHelpers {
+
     [ModelBinder(BinderType = typeof(DataSourceLoadOptionsBinder))]
-    public class DataSourceLoadOptions : DataSourceLoadOptionsBase
-    {
+    public class DataSourceLoadOptions : DataSourceLoadOptionsBase {
     }
 
-    public class DataSourceLoadOptionsBinder : IModelBinder
-    {
+    public class DataSourceLoadOptionsBinder : IModelBinder {
 
-        public Task BindModelAsync(ModelBindingContext bindingContext)
-        {
+        public Task BindModelAsync(ModelBindingContext bindingContext) {
             var loadOptions = new DataSourceLoadOptions();
             DataSourceLoadOptionsParser.Parse(loadOptions, key => bindingContext.ValueProvider.GetValue(key).FirstOrDefault());
             bindingContext.Result = ModelBindingResult.Success(loadOptions);
-            return null;
+            return Task.CompletedTask;
         }
+
     }
 
     [Obsolete("This attribute is now obsolete and is no longer used. It was used as a workaround for the issue discussed at https://github.com/aspnet/Mvc/issues/4652")]
-    public class DataSourceLoadOptionsAttribute : ModelBinderAttribute
-    {
+    public class DataSourceLoadOptionsAttribute : ModelBinderAttribute {
 
-        public DataSourceLoadOptionsAttribute()
-        {
+        public DataSourceLoadOptionsAttribute() {
             BinderType = typeof(DataSourceLoadOptionsBinder);
         }
+
     }
+
 }
